a16578dba634be70449ef302e321246f7c71586c
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-05-22 22:08:10 -0700

    fixed conflict

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -8,9 +8,6 @@ router.route('/').get(getUsers).post(createUser);
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
-<<<<<<< HEAD
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend)
-=======
->>>>>>> 073eb853c479c4f8aef26a89455c66ea2a7a71e3
 
 module.exports = router;
